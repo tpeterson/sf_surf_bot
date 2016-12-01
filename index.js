7ab8b2be5da3d26d5e293ee60875d42da820faa9
Tim <tpeterson@users.noreply.github.com>
2016-11-30 19:02:17 -0800

    Fix bug with calling this in getSurfreport intent and validate desired request parameters

--- index.js
+++ index.js
@@ -12,32 +12,39 @@ exports.handler = function(event, context, callback) {
 
 var handlers = {
     'LaunchRequest': function () {
-        this.attributes['speechOutput'] = 'You can tell Alexa to ask S.F. Surf Bot how are the waves at Mavericks or Ocean Beach or Pacifica.';
+        this.attributes['speechOutput'] = 'You can tell Alexa to ask Bay Area Surf Bot how are the waves at Mavericks or Ocean Beach or Pacifica.';
         this.attributes['repromptSpeech'] = 'Where are you looking to surf?';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
     'getSurfreport': function() {
-      var itemSlot = this.event.request.intent.slots.SURFSPOT;
-      var locationName = locationName = itemSlot.value;
+      var echo = this;
 
-      if (_.isEmpty(locationName)) {
-        this.attributes['speechOutput'] = 'Sorry, I didn\'t catch that.';
-        this.attributes['repromptSpeech'] = 'Where are you looking to surf?';
-        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
-      } else {
-        var echo = this;
-        var surfHelper = new SurfDataHelper();
-        surfHelper.whichSpot(locationName).then(function(report) {
-          echo.emit(':tell', report);
-        }).catch(function(err) {
-          echo.attributes['speechOutput'] = `I can\'t get a surf report for ${locationName || 'that spot'}`;
+      if (echo.event.request.intent.name === 'getSurfreport' && echo.event.request.intent.slots.hasOwnProperty('SURFSPOT')) {
+        var itemSlot = echo.event.request.intent.slots.SURFSPOT;
+        var locationName = itemSlot.hasOwnProperty('value') ? itemSlot.value : false;
+
+        if (!locationName) {
+          echo.attributes['speechOutput'] = 'Sorry, I missed what you said.';
           echo.attributes['repromptSpeech'] = 'Where are you looking to surf?';
           echo.emit(':ask', echo.attributes['speechOutput'], echo.attributes['repromptSpeech']);
-        });
+        } else {
+          var surfHelper = new SurfDataHelper();
+          surfHelper.whichSpot(locationName).then(function(report) {
+            echo.emit(':tell', report);
+          }).catch(function(err) {
+            echo.attributes['speechOutput'] = `I can\'t get a surf report for ${locationName || 'that spot'}`;
+            echo.attributes['repromptSpeech'] = 'Where are you looking to surf?';
+            echo.emit(':ask', echo.attributes['speechOutput'], echo.attributes['repromptSpeech']);
+          });
+        }
+      } else {
+        echo.attributes['speechOutput'] = 'Sorry, I didn\'t catch that.';
+        echo.attributes['repromptSpeech'] = 'Where are you looking to surf?';
+        echo.emit(':ask', echo.attributes['speechOutput'], echo.attributes['repromptSpeech']);
       }
     },
     'AMAZON.HelpIntent': function () {
-        this.attributes['speechOutput'] = 'You can tell Alexa to ask S.F. Surf Bot how are the waves at Mavericks or Ocean Beach or Pacifica.';
+        this.attributes['speechOutput'] = 'You can tell Alexa to ask Bay Area Surf Bot how are the waves at Mavericks or Ocean Beach or Pacifica.';
         this.attributes['repromptSpeech'] = 'Where are you looking to surf?';
         this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech']);
     },
